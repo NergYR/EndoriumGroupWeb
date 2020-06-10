8e704496099a847c8df9619233ab84b3618c2acc
Energetiq <46401261+NergYR@users.noreply.github.com>
2020-06-10 14:47:02 +0200

    Sys Sending Email

--- conta.php
+++ conta.php
@@ -1,8 +1,19 @@
 <?php
 
-session_start()
+session_start();
 
-$offer = $_POST['name']
+$name = $_POST['firstname'];
+$lastname = $_POST['lastname'];
+$mail = $_POST['mail'];
+$entreprise = $_POST['entreprise'];
+$phone = $_POST['phone'];
+$city = $_POST['city'];
+$depa = $_POST['depa'];
+
+echo "Voici vos Informations, '$name', '$lastname', '$mail', '$entreprise', '$phone', '$city', '$depa' .";
+
+mail("enegetiq@outlook.com", "New Client", "Voici les Informations de votre Client, prenom : '$name', nom : '$lastname', mail : '$mail', entreprise : '$entreprise', telephone : '$phone', Ville : '$city', Departement : '$depa' ");
+
 
     
 
